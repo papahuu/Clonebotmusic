dee3717cd762f2b708f76c0ab489e166e4f83f3d
TheRadhe <rajveer202415@gmail.com>
2025-04-10 08:19:43 +0530

    Update repo.py

--- Clonify/plugins/bot/repo.py
+++ Clonify/plugins/bot/repo.py
@@ -26,8 +26,8 @@ __Fᴏʀᴋ ɪᴛ, ᴄᴜsᴛᴏᴍɪᴢᴇ ɪᴛ, ᴀɴᴅ ᴍᴀᴋᴇ ɪᴛ �
 async def start(_, msg):
     buttons = [
         [
-                InlineKeyboardButton("𝗦𝗨𝗣𝗣𝗢𝗥𝗧", url="https://t.me/ProBotGc"),
-                InlineKeyboardButton("𝗨𝗣𝗗𝗔𝗧𝗘", url="https://t.me/ProBotts")
+                InlineKeyboardButton("𝗦𝗨𝗣𝗣𝗢𝗥𝗧", url="https://t.me/BOT_SUPPORT_GROUP7"),
+                InlineKeyboardButton("𝗨𝗣𝗗𝗔𝗧𝗘", url="https://t.me/ll_BOTCHAMBER_ll")
         ],
         [ 
           InlineKeyboardButton("𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘", url=f"https://github.com/TeamProBots/Clonify")
@@ -39,7 +39,7 @@ async def start(_, msg):
     await msg.reply_photo(
         photo="https://i.ibb.co/gFm6VW52/source-code.jpg",
         caption=start_txt,
-        reply_markup=reply_markup
+        reply_markup=reply_
     )
  
    
@@ -60,7 +60,7 @@ async def repo(_, message):
             list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
             count += 1
 
-        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://t.me/ProBotts) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/ProBotGc)
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://t.me/ll_BOTCHAMBER_ll) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/BOT_SUPPORT_GROUP7)
 | 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
 ----------------
 {list_of_users}"""
